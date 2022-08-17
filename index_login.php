dc339db3f9c2be436d86507b663d93ae82cbaf69
nkiru-emeka <nkiruemekaoti@gmail.com>
2022-08-17 16:01:56 +0100

    update

---
+++ index_login.php
@@ -0,0 +1,32 @@
+<?php 
+if (isset($_POST['submit'])){
+    $a  $_POST['username'];
+    $b= $_POST['password'];
+    
+    
+    
+    }
+    
+
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Login</title>
+</head>
+
+<body>
+	
+        <table>
+            <tr>
+                <td>
+                <form method="post" action="" enctype="multipart/form-data">
+                </td>
+            </tr>
+        </table>
+ 
+
+</body>
+</html>
